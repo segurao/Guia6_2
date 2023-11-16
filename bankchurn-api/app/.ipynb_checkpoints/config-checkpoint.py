ab9e2363ca515996387a1de44122ce8f75605bcd
segurao <g.segurao@uniandes.edu.co>
2023-11-15 19:26:11 -0500

    Camvio del nombre

--- bankchurn-api/app/.ipynb_checkpoints/config-checkpoint.py
+++ bankchurn-api/app/.ipynb_checkpoints/config-checkpoint.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Andrea Segura"
+    PROJECT_NAME: str = "Banckchurn API"
 
     class Config:
         case_sensitive = True
